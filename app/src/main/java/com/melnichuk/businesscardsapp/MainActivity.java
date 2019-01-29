e9e70bc331af9f7c4081f489a74807c1051627ec
Yegor Melnichuk <myegor15@gmail.com>
2019-01-29 20:03:09 +0200

    Added Toolbar, TabLayout and ViewPager to layout_main.xml; added menu.xml, fragment_example.xml, TabsFragmentAdapter.class, ExampleAdapter.class; updated MainActivity.class

--- app/src/main/java/com/melnichuk/businesscardsapp/MainActivity.java
+++ app/src/main/java/com/melnichuk/businesscardsapp/MainActivity.java
@@ -1,13 +1,51 @@
 package com.melnichuk.businesscardsapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.melnichuk.businesscardsapp.adapter.TabsFragmentAdapter;
+import com.melnichuk.businesscardsapp.fragment.ExampleFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int LAYOUT = R.layout.activity_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
+
+        initToolbar();
+        initTabs();
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.inflateMenu(R.menu.menu);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+    }
+
+    private void initTabs() {
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        // adding fragments
+        TabsFragmentAdapter adapter = new TabsFragmentAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ExampleFragment(), getString(R.string.cardsTabTitle));
+        adapter.addFragment(new ExampleFragment(), getString(R.string.myCardsTabTitle));
+
+        // setup
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
---
+++ app/src/main/java/com/melnichuk/businesscardsapp/adapter/TabsFragmentAdapter.java
@@ -0,0 +1,40 @@
+package com.melnichuk.businesscardsapp.adapter;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TabsFragmentAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private final List<String> fragmentTitleList = new ArrayList<>();
+
+    public TabsFragmentAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitleList.get(position);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragmentList.add(fragment);
+        fragmentTitleList.add(title);
+    }
+}
