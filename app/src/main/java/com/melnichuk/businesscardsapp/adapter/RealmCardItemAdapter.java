cbba7bb883cec683195b10eef32cc827b5590864
user <myegor15@gmail.com>
2019-11-13 18:04:35 +0200

    Added Firebase to project and migrated to AndroidX

--- app/src/main/java/com/melnichuk/businesscardsapp/adapter/RealmCardItemAdapter.java
+++ app/src/main/java/com/melnichuk/businesscardsapp/adapter/RealmCardItemAdapter.java
@@ -1,14 +1,12 @@
 package com.melnichuk.businesscardsapp.adapter;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.melnichuk.businesscardsapp.dialog.CardDialog;
 import com.melnichuk.businesscardsapp.R;
